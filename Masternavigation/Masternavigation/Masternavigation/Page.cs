64ce2d90015f0ca7b018bcc8c95670a1517b2997
Enthusiastic_coder <21123907+shubhambaghel34@users.noreply.github.com>
2018-01-23 13:04:13 +0530

    Master navigation app and tabbed app
    Use with Api 19 level simulator

---
+++ Masternavigation/Masternavigation/Masternavigation/Page.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Masternavigation
+{
+    public class Page : ContentPage
+    {
+        Button b1;
+        Label l1;
+        public Page()
+        {
+           l1= new Label
+            {
+
+                Text = "click here to navigate page ",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Start
+            };
+            b1 = new Button
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+            Content = new StackLayout
+            {
+                Children = {
+                    b1,
+                    l1
+                   
+                  
+                }
+            };
+            b1.Clicked += b1_Clicked;
+        }
+
+        private void b1_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MasterPage());
+        }
+    }
+}
